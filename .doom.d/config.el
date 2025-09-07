9dbbdeb533ee9735babf25efaff836b431776e8e
iqbal <iqbal>
2025-09-07 13:44:40 +0530

    new tmux and doom config

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -68,7 +68,7 @@
 (map!
  ;; press v multiple time to expand region
  :v "v" #'er/expand-region
- ;; 
+ ;;
  ;; :n "=" #'my-indent-line
  )
 ;; (map! :leader
@@ -96,7 +96,33 @@
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
-
+(after! org
+  (setq org-capture-templates
+       '(("t" "Personal todo" entry (file+headline +org-capture-todo-file "Inbox")
+          "* [ ] %?\n%i\n" :prepend t)
+         ("n" "Personal notes" entry (file+headline +org-capture-notes-file "Inbox")
+          "* %u %?\n%i\n" :prepend t)
+         ("j" "Journal" entry (file+olp+datetree +org-capture-journal-file)
+          "* %U %?\n%i\n" :prepend t)
+         ("p" "Templates for projects")
+         ("pt" "Project-local todo" entry
+          (file+headline +org-capture-project-todo-file "Inbox") "* TODO %?\n%i\n"
+          :prepend t)
+         ("pn" "Project-local notes" entry
+          (file+headline +org-capture-project-notes-file "Inbox") "* %U %?\n%i\n%a"
+          :prepend t)
+         ("pc" "Project-local changelog" entry
+          (file+headline +org-capture-project-changelog-file "Unreleased")
+          "* %U %?\n%i\n%a" :prepend t)
+         ("o" "Centralized templates for projects")
+         ("ot" "Project todo" entry #'+org-capture-central-project-todo-file
+          "* TODO %?\n %i\n %a" :heading "Tasks" :prepend nil)
+         ("on" "Project notes" entry #'+org-capture-central-project-notes-file
+          "* %U %?\n %i\n %a" :heading "Notes" :prepend t)
+         ("oc" "Project changelog" entry #'+org-capture-central-project-changelog-file
+          "* %U %?\n %i\n %a" :heading "Changelog" :prepend t)))
+  (setq org-agenda-files
+        '("~/roam")))
  ;; (use-package org-roam
  ;;   :ensure t
  ;;   :custom
